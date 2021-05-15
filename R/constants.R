1f46ae952af77c4079af2c2683ae06374089ae15
mrkaye97 <mrkaye97@gmail.com>
2021-05-15 18:35:14 -0400

    refactor fitbit.R into constants.R

---
+++ R/constants.R
@@ -0,0 +1,2 @@
+url_base <- "https://api.fitbit.com/1/"
+url_api <- paste0(url_base, "user/-/")
--- R/fitbitr.R
+++
@@ -1,8 +0,0 @@
-#' fitbitr
-#'
-#' Provides an API Access to fitbit API via R
-#'
-#' @name fitbitr
-#' @docType package
-url_base <- "https://api.fitbit.com/1/"
-url_api <- paste0(url_base, "user/-/")
