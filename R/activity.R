fba94e598be5223cd3540fe31d75f6556bd66167
mrkaye97 <mrkaye97@gmail.com>
2021-05-16 12:20:21 -0400

    more updates

--- R/activity.R
+++ R/activity.R
@@ -1,231 +1,131 @@
-#' @include fitbitr.R
-# Constants
 url_activity <- paste0(url_api, "activities/")
 
-#' @title Get Daily Activity Summary
-#'
-#' @description
-#'   \code{get_activity_summary()} retrieves a summary and list of a user's activities for a given day.
+#' @title Daily Activity Summary
 #'
 #' @details
-#' See \url{https://dev.fitbit.com/reference/web-api/activity/#get-daily-activity-summary} for more details.
-#'
+#' See \url{https://dev.fitbit.com/build/reference/web-api/activity/} for more details.
+#' @param date The date of records to be returned in "yyyy-mm-dd" or date(time) format
+#' @param token Fitbit access token
+#' @param user_id Fitbit user id
 #' @export
-get_activity_summary <- function(token, date, simplify = TRUE) {
-  url <- paste0(url_activity, sprintf("date/%s.json", format_date(date)))
-  # We can not simplify this output because it is so complicated nested list
-  tidy_output(get(url, token), simplify = FALSE)
-}
+get_activity_summary <- function(date, token = Sys.getenv("FITBIT_ACCESS_TOKEN"), user_id = Sys.getenv("FITBIT_USER_ID")) {
 
-#' @title Get Activity Log
-#'
-#' @description
-#'   \code{get_activity_log()} retrieves a data.frame of a user's activity log entries for a given day.
-#'
-#'
-#' @details
-#' See \url{https://dev.fitbit.com/build/reference/web-api/activity/#get-activity-logs-list} for more details.
-#'
-#' @export
-get_activity_log <- function(token, date = Sys.Date(), simplify = TRUE) {
-  url <- paste0(
-    url_activity, "list.json?beforeDate=",
-    sprintf(format_date(as.Date(format_date(date)) + 1)),
-    "&sort=desc&offset=0&limit=20"
+  date_conv <- paste0('/', as.Date(date))
+
+  url <- paste0(url_activity, 'date', date_conv, '.json')
+  url <- gsub('user/-/', paste0("user/", user_id, "/"), url)
+
+  # We can not simplify this output because it is so complicated nested list
+  r <- get(
+    url = url,
+    token = token
   )
-  re <- tidy_output(get(url, token), simplify = FALSE)$activities
-  if (is.data.frame(re)) {
-    re[re$originalStartTime == as.Date(date), ]
-  } else {
-    print("No logging for this date")
-  }
+
+  r %>%
+    content() %>%
+    pluck('summary') %>%
+    list_modify('heartRateZones' = NULL) %>%
+    list_modify('distances' = NULL) %>%
+    bind_rows() %>%
+    mutate(
+      date = date
+    ) %>%
+    select(date, everything())
 }
 
-#' @title Get Activity Time Series
-#'
-#' @description
-#'   \code{get_activity()} returns time series data in the specified range for a given resource.
-#'
-#' @param resource_path The resource path. see details below.
-#' @param base_date The range start date. A Date class object or a string in the format yyyy-MM-dd or today.
-#' @param end_date The end date of the range. A Date class object or a string in the format yyyy-MM-dd.
-#' @param date The end date of the period specified. A Date class object or a string in the format yyyy-MM-dd.
-#' @param period The range for which data will be returned. Options are "1d", "7d", "30d", "1w", "1m", "3m", "6m", "1y", or "max".
+#' @title Heart Rate Zones
 #'
 #' @details
-#'  Available resource_path are
-#'  \itemize{
-#'    \item calories
-#'    \item caloriesBMR
-#'    \item steps
-#'    \item distance
-#'    \item floors
-#'    \item elevation
-#'    \item minutesSedentary
-#'    \item minutesLightlyActive
-#'    \item minutesFairlyActive
-#'    \item minutesVeryActive
-#'    \item activityCalories
-#'  }
-#'
-#'  See \url{https://dev.fitbit.com/reference/web-api/activity/#get-activity-time-series} for more details.
-#'
+#' See \url{https://dev.fitbit.com/build/reference/web-api/activity/} for more details.
+#' @param date The date of records to be returned in "yyyy-mm-dd" or date(time) format
+#' @param token Fitbit access token
+#' @param user_id Fitbit user id
 #' @export
-get_activity <- function(token, resource_path, date = "", period = "", base_date = "", end_date = "", simplify = TRUE) {
-  url <- if (date != "" && period != "") {
-    paste0(url_activity, sprintf("%s/date/%s/%s.json", resource_path, format_date(date), period))
-  } else if (base_date != "" & end_date != "") {
-    paste0(url_activity, sprintf("%s/date/%s/%s.json", resource_path, format_date(base_date), format_date(end_date)))
-  } else {
-    stop("Error: Need to enter combination of date/period or base_date/end_date")
-  }
-  tidy_output(get(url, token), simplify)
-}
+get_heart_rate_zones <- function(date, token = Sys.getenv("FITBIT_ACCESS_TOKEN"), user_id = Sys.getenv("FITBIT_USER_ID")) {
 
-#' @title Get Activity Intraday Time Series
-#'
-#' @description
-#'   \code{get_activity_intraday()} returns intraday time series data in the specified range for a given resource.
-#'   Access to the Intraday Time Series for personal use (accessing your own data) is available through the "Personal" App Type.
-#'
-#' @param resource_path The resource path of the desired data
-#' @param detail_level Number of data points to include. Either 1min or 15min. Optional.
-#' @param start_time The start of the period, in the format HH:mm. Optional.
-#' @param end_time The end of the period, in the format HH:mm. Optional.
-#'
-#' @details
-#'  Available resource_path are
-#'  \itemize{
-#'    \item calories
-#'    \item steps
-#'    \item distance
-#'    \item floors
-#'    \item elevation
-#'  }
-#'
-#'  See \url{https://dev.fitbit.com/reference/web-api/activity/#get-activity-intraday-time-series} for more details.
-#'
-#' @export
-get_activity_intraday <- function(token, resource_path, date, detail_level = "15min", start_time = NULL, end_time = NULL, simplify = TRUE) {
-  date <- format_date(date)
-  url <- if (!is.null(start_time) && !is.null(end_time)) {
-    date2 <- if (start_time < end_time) {
-      "1d"
-    } else {
-      date2 <- as.Date(date) + 1
-    }
-    paste0(url_activity, sprintf("%s/date/%s/%s/%s/time/%s/%s.json", resource_path, date, date2, detail_level, start_time, end_time))
-  } else {
-    paste0(url_activity, sprintf("%s/date/%s/1d/%s.json", resource_path, date, detail_level))
-  }
-  tidy_output(get(url, token), simplify)
-}
+  date_conv <- paste0('/', as.Date(date))
 
-#' @title Get Activity Types
-#'
-#' @description
-#'   Get a tree of all valid Fitbit public activities from the activities catalog as well as private custom activities the user created
-#'
-#'
-#' @export
-get_activity_types <- function(token, simplify = TRUE) {
-  url <- paste0(url_activity, "activities.json")
+  url <- paste0(url_activity, 'date', date_conv, '.json')
+  url <- gsub('user/-/', paste0("user/", user_id, "/"), url)
+
   # We can not simplify this output because it is so complicated nested list
-  tidy_output(get(url, token), simplify = FALSE)
+  r <- get(
+    url = url,
+    token = token
+  )
+
+  r %>%
+    content() %>%
+    pluck('summary') %>%
+    pluck('heartRateZones') %>%
+    bind_rows() %>%
+    mutate(
+      date = date
+    ) %>%
+    select(date, everything())
 }
 
-#' @title Get Activity Type
-#'
-#' @description
-#'   \code{get_activity_type()} returns the details of a specific activity in the Fitbit activities database.
-#'
-#' @param activity_id The activity ID.
-#'
-#' @export
-get_activity_type <- function(token, activity_id, simplify = TRUE) {
-  url <- paste0(url_base, sprintf("activities/%s.json", activity_id))
-  tidy_output(get(url, token), simplify)
-}
-
-#' @title Add Favorite Activity
-#'
-#' @description
-#'   \code{add_favorite_activity()} adds the activity with the given ID to user's list of favorite activities.
-#'
-#' @param token An OAuth 2.0 token generated by oauth_token()
-#' @param activity_id	The ID of the activity to add to user's favorites.
+#' @title Distances
 #'
 #' @details
-#'   See \url{https://dev.fitbit.com/reference/web-api/activity/#add-favorite-activity} for more details.
-#'
+#' See \url{https://dev.fitbit.com/build/reference/web-api/activity/} for more details.
+#' @param date The date of records to be returned in "yyyy-mm-dd" or date(time) format
+#' @param token Fitbit access token
+#' @param user_id Fitbit user id
 #' @export
-add_favorite_activity <- function(token, activity_id) {
-  # POST https://api.fitbit.com/1/user/-/activities/favorite/[activity-id].json
-  url <- paste0(url_activity, sprintf("favorite/%s.json", activity_id))
-  invisible(post(url, token, body = NULL))
+get_distances <- function(date, token = Sys.getenv("FITBIT_ACCESS_TOKEN"), user_id = Sys.getenv("FITBIT_USER_ID")) {
+
+  date_conv <- paste0('/', as.Date(date))
+
+  url <- paste0(url_activity, 'date', date_conv, '.json')
+  url <- gsub('user/-/', paste0("user/", user_id, "/"), url)
+
+  # We can not simplify this output because it is so complicated nested list
+  r <- get(
+    url = url,
+    token = token
+  )
+
+  r %>%
+    content() %>%
+    pluck('summary') %>%
+    pluck('distances') %>%
+    bind_rows() %>%
+    mutate(
+      date = date
+    ) %>%
+    select(date, everything())
 }
 
-#' @title Delete Favorite Activity
-#'
-#' @description
-#'   \code{delete_favorite_activity()} removes the activity with the given ID (activity_id) from a user's list of favorite activities.
-#'
-#' @param activity_id The ID of the activity to be removed.
+#' @title Activity Time Series
 #'
 #' @details
-#'   See \url{https://dev.fitbit.com/reference/web-api/activity/#delete-favorite-activity} for more details.
-#'
+#' See \url{https://dev.fitbit.com/build/reference/web-api/activity/} for more details.
+#' @param start_date The start date of records to be returned in "yyyy-mm-dd" or date(time) format
+#' @param end_date The end date of records to be returned in "yyyy-mm-dd" or date(time) format
+#' @param resource_path The resource path. See \url{https://dev.fitbit.com/build/reference/web-api/activity/} for options
+#' @param token Fitbit access token
+#' @param user_id Fitbit user id
+#' @importFrom purrr flatten_dfr
 #' @export
-delete_favorite_activity <- function(token, activity_id) {
-  url <- paste0(url_activity, sprintf("favorite/%s.json", activity_id))
-  invisible(delete(url, token))
-}
+get_activity_time_series <- function(start_date, end_date, resource_path, token = Sys.getenv("FITBIT_ACCESS_TOKEN"), user_id = Sys.getenv("FITBIT_USER_ID")) {
 
-#' @title Get Activity Goals
-#'
-#' @description
-#'   \code{get_activity_goals()} retrieves a user's current daily or weekly activity goals
-#'
-#' @param period "daily" or "weekly"
-#'
-#' @details
-#' See \url{https://dev.fitbit.com/reference/web-api/activity/#get-activity-goals} for more details.
-#'
-#' @export
-get_activity_goals <- function(token, period, simplify = TRUE) {
-  stop_if_x_is_not_in(period, c("daily", "weekly"))
+  if (grepl('activities/', resource_path)) resource_path <- gsub('activities/', '', resource_path)
 
-  url <- paste0(url_activity, sprintf("goals/%s.json", period))
-  tidy_output(get(url, token), simplify)
-}
+  start_date_conv <- paste0('/', as.Date(start_date))
+  end_date_conv <- paste0('/', as.Date(end_date))
 
-#' @title Update Activity Goals
-#'
-#' @description
-#'   \code{update_activity_goals()} creates or updates a user's daily activity goals.
-#'
-#' @param period daily or weekly
-#' @param calories_out optional	Goal value; integer.
-#' @param active_minutes optional	Goal value; integer.
-#' @param floors optional	Goal value; integer.
-#' @param distance optional	Goal value; in the format X.XX or integer.
-#' @param steps optional	Goal value; integer.
-#' @importFrom purrr map
-#' @importFrom rlang set_names eval_tidy sym
-#' @importFrom stringr str_to_upper
-#'
-#' @details
-#'   See \url{https://dev.fitbit.com/reference/web-api/activity/#update-activity-goals} for more details.
-#'
-#' @export
-update_activity_goals <- function(token, period, calories_out = NULL, active_minutes = NULL, floors = NULL, distance = NULL, steps = NULL) {
-  # POST https://api.fitbit.com/1/user/[user-id]/activities/goals/[period].json
-  url <- paste0(url_activity, sprintf("goals/%s.json", period))
-  post_arguments <- c("calories_out", "active_minutes", "floors", "distance", "steps")
-  body <- map(set_names(post_arguments), ~ eval_tidy(sym(.x)))
-  names(body) <- str_replace_all(names(body), "(_)([a-z])", function(x) {
-    str_to_upper(str_sub(x, 2))
-  })
-  tidy_output(post(url, token, body), simplify = TRUE)
+  url <- paste0(url_activity, resource_path, '/date', start_date_conv, end_date_conv, '.json')
+  url <- gsub('user/-/', paste0("user/", user_id, "/"), url)
+
+  # We can not simplify this output because it is so complicated nested list
+  r <- get(
+    url = url,
+    token = token
+  )
+
+  r %>%
+    content() %>%
+    flatten_dfr()
 }
 
--- R/sleep.R
+++ R/sleep.R
@@ -1,10 +1,10 @@
-# Constants
 url_sleep <- paste0(url_api, "sleep/")
 
 #' Nightly Sleep Summary
 #'
 #' Returns a single row tibble of summary data on the night of sleep
-#' @param date The date of records to be returned in "yyyy-mm-dd" or date(time) format
+#' @param start_date The start date of records to be returned in "yyyy-mm-dd" or date(time) format
+#' @param end_date The end date of records to be returned in "yyyy-mm-dd" or date(time) format
 #' @param token Fitbit access token
 #' @param user_id Fitbit user id
 #' @importFrom purrr pluck list_modify
@@ -34,40 +34,3 @@ get_sleep_summary <- function(start_date, end_date = NULL, token = Sys.getenv("F
     bind_rows() %>%
     arrange(dateOfSleep)
 }
-
-#' Get Sleep Time Series
-#'
-#' Returns time series data in the specified range for a given resource in the format requested.
-#' Note: Even if you provide earlier dates in the request, the response retrieves only data since the user's join date or the first log entry date for the requested collection.
-#'
-#' @param token An OAuth 2.0 token
-#' @param resource_path	The resource path; see the Resource Path Options below for a list of options.
-#' @param date	The end date of the period specified in the format "yyyy-MM-dd" or "today" as a string or Date object.
-#' @param period	The range for which data will be returned. Options are 1d, 7d, 30d, 1w, 1m, 3m, 6m, 1y, or max.
-#' @param base_date	The range start date in the format "yyyy-MM-dd" or "today" as a string or Date object.
-#' @param end_date	The end date of the range in the format "yyyy-MM-dd" or "today" as a string or Date object.
-#' @details Resource Path Options in more detail.
-#' \itemize{
-#'   \item{startTime}
-#'   \item{timeInBed}
-#'   \item{minutesAsleep}
-#'   \item{awakeningsCount}
-#'   \item{minutesAwake}
-#'   \item{minutesToFallAsleep}
-#'   \item{minutesAfterWakeup}
-#'   \item{efficiency}
-#' }
-#'
-#' @export
-get_sleep <- function(token, resource_path, date = "", period = "", base_date = "", end_date = "") {
-  url <- if (date != "" && period != "") {
-    paste0(url_sleep, sprintf("%s/date/%s/%s.json", resource_path, as.Date(date), period))
-  } else if (base_date != "" & end_date != "") {
-    paste0(url_sleep, sprintf("date/%s/%s.json", as.Date(base_date), as.Date(end_date)))
-  }
-
-  url <- gsub('user/-/', paste0("user/", user_id, "/"), url)
-
-  x <- content(get(url, token))
-}
-
