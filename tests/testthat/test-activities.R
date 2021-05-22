bf0d3e279a64c2e0749e484fb99efb04efaf8291
mrkaye97 <mrkaye97@gmail.com>
2021-05-22 14:35:51 -0400

    add some tests

---
+++ tests/testthat/test-activities.R
@@ -0,0 +1,118 @@
+start_date <- date <- '2021-05-21'
+end_date <- '2021-05-22'
+
+test_that('Activites summary downloads', {
+
+  skip_on_cran()
+  skip_on_ci()
+
+  tmp <- activity_summary(date)
+
+  expect_equal(nrow(tmp), 1)
+  expect_equal(ncol(tmp), 14)
+  expect_equal(colnames(tmp)[1], 'date')
+})
+
+test_that('Activity calories downloads', {
+
+  skip_on_cran()
+  skip_on_ci()
+
+  tmp <- activity_calories(start_date, end_date)
+
+  expect_equal(nrow(tmp), 2)
+  expect_equal(ncol(tmp), 2)
+  expect_equal(colnames(tmp), c('date', 'activityCalories'))
+})
+
+test_that('BMR calories downloads', {
+
+  skip_on_cran()
+  skip_on_ci()
+
+  tmp <- calories_bmr(start_date, end_date)
+
+  expect_equal(nrow(tmp), 2)
+  expect_equal(ncol(tmp), 2)
+  expect_equal(colnames(tmp), c('date', 'caloriesBMR'))
+})
+
+test_that('Total calories downloads', {
+
+  skip_on_cran()
+  skip_on_ci()
+
+  tmp <- calories(start_date, end_date)
+
+  expect_equal(nrow(tmp), 2)
+  expect_equal(ncol(tmp), 2)
+  expect_equal(colnames(tmp), c('date', 'calories'))
+})
+
+test_that('Distance downloads', {
+
+  skip_on_cran()
+  skip_on_ci()
+
+  tmp <- distance(start_date, end_date)
+
+  expect_equal(nrow(tmp), 2)
+  expect_equal(ncol(tmp), 2)
+  expect_equal(colnames(tmp), c('date', 'distance'))
+})
+
+test_that('Elevation downloads', {
+
+  skip_on_cran()
+  skip_on_ci()
+
+  tmp <- elevation(start_date, end_date)
+
+  expect_equal(nrow(tmp), 2)
+  expect_equal(ncol(tmp), 2)
+  expect_equal(colnames(tmp), c('date', 'elevation'))
+})
+
+test_that('Floors downloads', {
+
+  skip_on_cran()
+  skip_on_ci()
+
+  tmp <- floors(start_date, end_date)
+
+  expect_equal(nrow(tmp), 2)
+  expect_equal(ncol(tmp), 2)
+  expect_equal(colnames(tmp), c('date', 'floors'))
+})
+
+test_that('Minutes downloads', {
+
+  skip_on_cran()
+  skip_on_ci()
+
+  sedentary <- minutes_sedentary(start_date, end_date)
+  lightly <- minutes_lightly_active(start_date, end_date)
+  fairly <- minutes_fairly_active(start_date, end_date)
+  very <- minutes_very_active(start_date, end_date)
+
+  list(
+    sedentary,
+    lightly,
+    fairly,
+    very
+  ) %>%
+    purrr::map(
+      ~ expect_equal(nrow(.x), 2)
+    )
+
+  list(
+    sedentary,
+    lightly,
+    fairly,
+    very
+  ) %>%
+    purrr::map(
+      ~ expect_equal(ncol(.x), 2)
+    )
+})
+
