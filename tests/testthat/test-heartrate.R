507f067bb5f0d9a7c97dc0a0c9d16ad090c3a7aa
mrkaye97 <mrkaye97@gmail.com>
2021-05-22 15:10:29 -0400

    add hr tests

---
+++ tests/testthat/test-heartrate.R
@@ -0,0 +1,32 @@
+date <- start_date <- '2021-05-20'
+end_date <- '2021-05-21'
+
+test_that("Heart rate by minute works", {
+  tmp <- heart_rate_intraday(
+    date = date,
+    minutes = TRUE
+  )
+
+  expect_equal(ncol(tmp), 2)
+  expect_equal(colnames(tmp), c('time', 'heart_rate'))
+  expect_gt(nrow(tmp), 100)
+})
+
+test_that("Heart rate by second works", {
+  tmp <- heart_rate_intraday(
+    date = date,
+    minutes = FALSE
+  )
+
+  expect_equal(ncol(tmp), 2)
+  expect_equal(colnames(tmp), c('time', 'heart_rate'))
+  expect_gt(nrow(tmp), 100)
+})
+
+test_that("Heart rate zones works", {
+  tmp <- heart_rate_zones(date)
+
+  expect_equal(ncol(tmp), 4)
+  expect_equal(nrow(tmp), 4)
+  expect_equal(colnames(tmp), c("date", "zone", "min_hr", "max_hr", "minutes_in_zone", "calories_out"))
+})
