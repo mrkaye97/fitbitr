3846ffff436588dbca4074b624106cc31593bcb1
mrkaye97 <mrkaye97@gmail.com>
2021-05-29 21:18:46 -0400

    brittle but passes r cmd check

--- tests/testthat/test-aaa-token-loads.R
+++ tests/testthat/test-aaa-token-loads.R
@@ -3,7 +3,9 @@ test_that('Token loads', {
   skip_on_cran()
   skip_on_ci()
 
-  load_cached_token('../../.httr-oauth')
+  cache_loc <- Sys.getenv("FITBITR_CACHE_LOC")
+
+  load_cached_token(cache_loc)
   tmp <- check_token_exists()
 
   expect_true(tmp)
